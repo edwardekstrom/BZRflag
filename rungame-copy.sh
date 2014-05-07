76aaa2fc6f9d30ddf566c30045c0c35ed30e304f
Edward Ekstrom <edward.ekstrom@gmail.com>
2014-05-07 13:05:38 -0600

    updated rungame-copy.sh

--- rungame-copy.sh
+++ rungame-copy.sh
@@ -2,8 +2,8 @@
 #./bin/bzrflag --world=maps/rotated_box_world.bzw  --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 2
 python bzagents/agent1_spencer.py localhost 50100 &
-sleep 2
-python bzagents/fields.py &
+# sleep 2
+# python bzagents/fields.py &
 # python bzagents/agent1_spencer.py localhost 50101 &
 # python bzagents/agent1_spencer.py localhost 50102 &
 # python bzagents/agent1_spencer.py localhost 50103 &
