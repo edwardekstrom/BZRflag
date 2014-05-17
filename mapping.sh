a882a52aba89a1000cf45c66333e602cb4a8e34d
Edward Ekstrom <edward.ekstrom@gmail.com>
2014-05-17 16:09:23 -0600

    updated shell scripts

--- mapping.sh
+++ mapping.sh
@@ -1,8 +1,8 @@
 ./bin/bzrflag --world=maps/four_ls.bzw --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 #./bin/bzrflag --world=maps/rotated_box_world.bzw --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 2
-python bzagents/pf_agent.py localhost 50100 &
+python bzagents/mapping_agent.py localhost 50100 &
 # python bzagents/dumb_agent.py localhost 50101 &
 python bzagents/dumb_agent.py localhost 50102 &
-python bzagents/pf_agent.py localhost 50103 &
+python bzagents/mapping_agent.py localhost 50103 &
 
